938c1e2209505220499e8c9e98a8db1c3e8e21e0
Pan_Ivan <ilitovko660@gmail.com>
2026-04-11 00:14:24 +0200

    обновил вид админки

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -34,6 +34,17 @@ ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', '').split('
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",  # before django.contrib.admin
+
+    "unfold.contrib.filters",        # optional, if special filters are needed
+    "unfold.contrib.forms",          # optional, if special form elements are needed
+    "unfold.contrib.inlines",        # optional, if special inlines are needed
+    "unfold.contrib.import_export",  # optional, if django-import-export package is used
+    "unfold.contrib.guardian",       # optional, if django-guardian package is used
+    "unfold.contrib.simple_history",
+
+
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
