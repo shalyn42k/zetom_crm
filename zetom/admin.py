261796d6b312741d298fa7dda0e17dddd5aa65fb
shalyn42k <tymirgundogar@gmail.com>
2026-04-11 22:15:01 +0200

    Update admin.py

--- zetom/admin.py
+++ zetom/admin.py
@@ -1,17 +1,26 @@
 from django import forms
 from django.contrib import admin
 from django.shortcuts import redirect
-
 from unfold.admin import ModelAdmin
 from unfold.decorators import action
 
-from .models import Request_Null
+from .forms import AddOferta, AddRequestFormMain, AddRequestFormNull
+from .models import Oferta, RequestMain, RequestNull, Role, UserProfile
 
 
-@admin.register(Request_Null)
-class Request_NullAdmin(ModelAdmin):      
-    list_display = ('created_at', 'phone', 'company_name', 'company_nip', 'email')
+@admin.register(Role)
+class AdminRole(admin.ModelAdmin):
+    list_display = ("code", "name", "level")
 
+@admin.register(UserProfile)
+class AdminUserProfile(admin.ModelAdmin):
+    list_display = ("user", "role")
+
+
+@admin.register(RequestNull)
+class RequestNullAdmin(admin.ModelAdmin):
+    form = AddRequestFormNull
+    list_display = ("created_at", "phone", "company_name", "company_nip", "email")
     actions_detail = ["oferta_action", "zlecenie_action"]
 
     @action(
@@ -31,3 +40,17 @@ class Request_NullAdmin(ModelAdmin):
     def zlecenie_action(self, request, object_id):
         self.message_user(request, "zlecenie")
         return redirect("admin:zetom_request_null_change", object_id)
+
+
+@admin.register(RequestMain)
+class RequestMainAdmin(admin.ModelAdmin):
+    form = AddRequestFormMain
+    list_display = ("created_at", "full_name", "address", "notes")
+    exclude = ["from_null"]
+
+
+@admin.register(Oferta)
+class OfertaAdmin(admin.ModelAdmin):
+    form = AddOferta
+    list_display = ("created_at", "price")
+    exclude = ["from_main"]
