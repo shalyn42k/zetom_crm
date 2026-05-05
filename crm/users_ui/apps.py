4dab5379ba8ed589929fda8075032af47ad274d0
bildd11 <volkovnikita1977@gmail.com>
2026-05-05 15:18:49 +0200

    refactor: remove users_ui and move all files to users

--- crm/users_ui/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersUiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "crm.users_ui"
-    verbose_name = "Users UI"
